cb0c583f701653f84e7fa8d8b73606af49a4f0fa
Rachel Crosley <rcrosley@ciri.com>
2018-06-07 09:22:41 -0800

    adding the ret of the working web app

---
+++ WebApplication1/Models/Sandwich.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Sandwich
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ WebApplication1/Models/SandwichContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public class SandwichContext : DbContext
+    {
+        public SandwichContext(DbContextOptions<SandwichContext> options) : base(options)
+        {
+        }
+        public DbSet<Sandwich> Sandwich { get; set; }
+    }
+}
