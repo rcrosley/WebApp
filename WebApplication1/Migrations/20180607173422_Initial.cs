72876ddbd087d5a4b067a07978e90212b3b31ad7
Rachel Crosley <rcrosley@ciri.com>
2018-06-07 16:02:18 -0800

    adding defualt sandwiches

---
+++ WebApplication1/Migrations/20180607173422_Initial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace WebApplication1.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sandwich",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Birthday = table.Column<DateTime>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Price = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sandwich", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sandwich");
+        }
+    }
+}
---
+++ WebApplication1/Models/SeedData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApplication1.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new SandwichContext(
+                serviceProvider.GetRequiredService<DbContextOptions<SandwichContext>>()))
+            {
+                if (context.Sandwich.Any())
+                {
+                    return;
+                }
+                context.Sandwich.AddRange(
+                    new Sandwich
+                    {
+                        Name = "BLT",
+                        Birthday = DateTime.Parse("1996-11-8"),
+                        Price = 5.00M
+                    },
+                    new Sandwich
+                    {
+                        Name = "Club",
+                        Birthday = DateTime.Parse("1492-2-2"),
+                        Price = 22.22M
+                    }
+                    );
+                context.SaveChanges();
+            }
+        }
+        
+    }
+}
